8b8f34fa54e2f481d02d00a9ca27360408350180
Kevin <459792@student.fontys.nl>
2022-10-18 19:50:46 +0200

    getpackage in working state.

--- packages/getpackage.py
+++ packages/getpackage.py
@@ -1,5 +1,6 @@
 from os import path, remove
 import subprocess
+import hashlib
 
 filePath = "packages.csv"
 
@@ -10,10 +11,10 @@ with open(filePath) as file:
         packageName = data[0]
         packageVersion = data[1]
         packageURL = data[2].replace('@',packageVersion) #implement @ replacement
-        packageMD5SUM = data[3]
+        packageMD5SUM = data[3].rstrip()
 
         cache = packageURL.rsplit('/', 1)[-1]
-        print(cache)
+        #print(cache)
 
         if not path.exists(cache):
             print("Downloading %s." % cache)
@@ -24,16 +25,16 @@ with open(filePath) as file:
             except:
                 print("Failed to download %s." % packageURL)
                 exit(1)
-            try:
-                cmd = ['md5sum', cache]
-                md5sum = subprocess.check_call(cmd)
-
-                if md5sum != packageMD5SUM: 
-                    remove(cache)
-                    print("Verification of %s failed! MD5 mismatch!" % cache) #potential man in the middle attack
-                    exit(1)
-            except:
-                print("Error, failed to retrieve md5sum of %s." % cache)
+     
+            with open(cache, 'rb') as downloadedFile:
+                data = downloadedFile.read()    
+                returnedMD5SUM = hashlib.md5(data).hexdigest()
+            
+            if returnedMD5SUM != packageMD5SUM:
+                remove(cache)
+                print("Verification of %s failed! MD5 mismatch!" % cache) #potential man in the middle attack
+                print("expected MD5: %s, got: %s." % (packageMD5SUM, returnedMD5SUM))
                 exit(1)
+
         else:
             print("%s already exists, skipping." % cache)
